fa1c5a57b38963aa89243d231d8edd3a611fb87b
Eugene-Khonich <ehonich@gmail.com>
2025-01-09 00:05:09 +0200

    finish

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import router from './routers/index.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 export const setupServer = () => {
   const app = express();
@@ -28,6 +29,7 @@ export const setupServer = () => {
   });
   app.use(router);
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.use('*', notFoundHandler);
   app.use(errorHandler);
 
